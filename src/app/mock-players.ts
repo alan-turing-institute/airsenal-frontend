b2430fabdfa79875158f54456df492b30cd38c31
nbarlowATI <nbarlow@turing.ac.uk>
2019-10-13 16:06:49 +0100

    remove obsolete service

--- src/app/mock-players.ts
+++
@@ -1,7 +0,0 @@
-import { Player } from './player';
-
-export const PLAYERS: Player[] = [
-    { id: 11, name: "Bob Bobson" },
-    { id: 12, name: "Alice Alison"},
-    { id: 13, name: "Colin Colison"}
-];
