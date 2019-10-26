909415cc6a760bbb49750011c66b70da94683fb7
nbarlowATI <nbarlow@turing.ac.uk>
2019-10-26 23:04:57 +0100

    remove obsolete components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PlayersComponent } from './players/players.component';
 import { SquadComponent } from './squad/squad.component';
 import { SquadSelectorComponent } from './squad-selector/squad-selector.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'players', component: PlayersComponent },
+    { path: '', redirectTo: '/squad-selector', pathMatch: 'full' },
     { path: 'squad-selector', component: SquadSelectorComponent },
     { path: 'squad', component: SquadComponent },
     { path: 'detail/:id', component: PlayerDetailComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { PlayersComponent } from './players/players.component';
 import { PlayerDetailComponent } from './player-detail/player-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HttpClientModule }    from '@angular/common/http';
 import { SquadComponent } from './squad/squad.component';
@@ -18,10 +16,8 @@ import { SquadSelectorComponent } from './squad-selector/squad-selector.componen
 @NgModule({
     declarations: [
 	AppComponent,
-	PlayersComponent,
 	PlayerDetailComponent,
 	MessagesComponent,
-	DashboardComponent,
 	SquadComponent,
 	SquadSelectorComponent
     ],
